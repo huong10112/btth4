443f597c60c5f9d4d6891fd8e86dd933ee3169a2
huong10112 <huonglan10112@gmail.com>
2024-12-20 19:39:02 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IssueController;
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('issues', IssueController::class);
